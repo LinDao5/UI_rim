87519a5747df6ac854f4b275b5d54a531ce1a6de
pinefirst@outlook.com <53974651+LinDao5@users.noreply.github.com>
2021-08-16 12:01:48 +0800

    add animation in home part

--- src/HomeScene.tsx
+++ src/HomeScene.tsx
@@ -1,12 +1,115 @@
 import {useNavigation} from '@react-navigation/native'
 import {useHeaderHeight} from '@react-navigation/stack'
-import React, {useState} from 'react'
-import {Platform, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native'
+import React, {useEffect, useRef, useState} from 'react'
+import {
+    Animated, Image,
+    ListRenderItemInfo,
+    Platform,
+    Pressable,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+import {FlatList} from 'react-native-gesture-handler'
 import Icon from 'react-native-vector-icons/MaterialIcons'
+import {AppImages} from '../res'
 import Config from './Config'
+import {showToast} from './util/action'
 
 
 interface Props{}
+
+const DEMOS = [
+    {
+        name: 'hotel',
+        background: AppImages.hotel_booking,
+        screenName: 'Hotel',
+    },
+    {
+        name: 'fitness_app',
+        background: AppImages.fitness_app,
+        screenName: '',
+    },
+    {
+        name: 'design_course',
+        background: AppImages.design_course,
+        screenName: '',
+    },
+    {
+        name: '',
+        background: undefined,
+        screenName: '',
+    },
+];
+
+interface ListItemProps {
+    data : ListRenderItemInfo<{name: string; background: any}>;
+    isGrid : boolean;
+    onScreenClicked : () => void;
+}
+
+const ListItem : React.FC<ListItemProps> = ({
+   data,
+   isGrid,
+   onScreenClicked,
+}) => {
+    const {index, item} = data;
+    const translateY = useRef<Animated.Value>(new Animated.Value(50)).current;
+    const opacity = useRef<Animated.Value>(new Animated.Value(0)).current;
+
+    if (item.name.length === 0){
+        return null;
+    };
+
+    useEffect(() => {
+       Animated.parallel([
+           Animated.timing(translateY, {
+               toValue: 0,
+               duration: 1000,
+               delay: index * (1000 /3),
+               useNativeDriver: true
+           }),
+           Animated.timing(opacity, {
+              toValue: 1,
+              duration: 1000,
+              delay: index * (1000 / 3),
+              useNativeDriver: true,
+           }),
+       ]).start() ;
+    });
+
+    return (
+      <Animated.View
+          style={{
+              margin: 6,
+              height: isGrid ? 120 : 250,
+              opacity,
+              transform:[{translateY}],
+          }}
+      >
+          <Image
+              source={item.background}
+              style={{flex: 1, aspectRatio: 1.5, borderRadius: 4} }
+              resizeMode="cover"
+          />
+          <Pressable
+              style={({pressed}) => [
+                  {
+                      flex: 1,
+                      borderRadius: 4,
+                      backgroundColor:"rgba(128,128,128,0.1)",
+                      ...StyleSheet.absoluteFillObject,
+                      opacity:!Config.isAndroid && pressed ? 0.6 : 1,
+                  },
+              ]}
+              android_ripple={{color:"rgba(128,128,128,0.3)"} }
+              onPress={onScreenClicked}
+          />
+      </Animated.View>
+    );
+}
 
 const HomeScene: React.FC<Props> = () => {
 
@@ -45,6 +148,28 @@ const HomeScene: React.FC<Props> = () => {
                    />
                </Pressable>
            </View>
+           <FlatList
+               key={isGrid ? "G" : "L"}
+               style={{marginTop: headerHeight, marginHorizontal: 6}}
+               contentContainerStyle={{flexGrow: 1}}
+               data={DEMOS}
+               keyExtractor={(item ) => item.name}
+               numColumns={isGrid ? 2 : 1}
+               renderItem={(data) => (
+                    <ListItem
+                        data={data}
+                        isGrid={isGrid}
+                        onScreenClicked={() => {
+                            if (data.item.screenName){
+                                navigation.navigate(data.item.screenName);
+                            }else {
+                                showToast("Coming soon...");
+                            }
+                        }}
+                    />
+               )}
+               showsVerticalScrollIndicator={false}
+           />
        </SafeAreaView>
     );
 };
