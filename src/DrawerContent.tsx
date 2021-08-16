57120dac6418c5892b022d7a3069fa255d24ffa6
pinefirst@outlook.com <53974651+LinDao5@users.noreply.github.com>
2021-08-16 10:54:51 +0800

    add toggleDrawer in home

--- src/DrawerContent.tsx
+++ src/DrawerContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
     StyleSheet,
     View,
@@ -13,10 +13,10 @@ import {
     DrawerContentOptions,
     DrawerContentScrollView,
 } from '@react-navigation/drawer';
-import { DrawerActions } from '@react-navigation/native';
+import {DrawerActions} from '@react-navigation/native';
 import Animated from 'react-native-reanimated';
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import { AppImages } from '../res';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {AppImages} from '../res';
 import Config from './Config';
 
 interface DrawerItemProps {
@@ -27,11 +27,10 @@ interface DrawerItemProps {
     onpress?: () => void | null | undefined;
 }
 
-const DrawerItemRow: React.FC<
-    DrawerItemProps & DrawerContentComponentProps<DrawerContentOptions>
-    > = (props) => {
-    const window = useWindowDimensions();
-    const rowWidth = (window.width * 0.75 * 80) / 100;
+
+const DrawerItemRow: React.FC<DrawerItemProps & DrawerContentComponentProps<DrawerContentOptions>> = (props) => {
+    const window = useWindowDimensions()
+    const rowWidth = (window.width * 0.75 * 80) / 100
     const {
         state,
         label,
@@ -39,15 +38,14 @@ const DrawerItemRow: React.FC<
         isAssetIcon = false,
         translateX,
         onpress,
-    } = props;
-    const { routes, index } = state;
-    const focused = getActiveRouteState(routes, index, label);
-
-    const tintColor = focused ? props.activeBackgroundColor : 'black';
+    } = props
+    const {routes, index} = state                         // todo how to know such as props
+    const focused = getActiveRouteState(routes, index, label)
+    const tintColor = focused ? props.activeBackgroundColor : 'black'
     return (
         <Pressable
             style={styles.drawerRowStyle}
-            android_ripple={{ color: 'lightgrey' }}
+            android_ripple={{color: 'lightgrey'}}
             onPress={onpress}
         >
             <Animated.View
@@ -58,7 +56,7 @@ const DrawerItemRow: React.FC<
                         backgroundColor: focused
                             ? props.activeBackgroundColor
                             : props.inactiveBackgroundColor,
-                        transform: [{ translateX }],
+                        transform: [{translateX}],
                     },
                 ]}
             />
@@ -66,125 +64,123 @@ const DrawerItemRow: React.FC<
                 {isAssetIcon ? (
                     <Image
                         source={icon}
-                        style={{ width: 24, height: 24, tintColor }}
+                        style={{width: 24, height: 24, tintColor}}
                         resizeMode="contain"
                     />
                 ) : (
-                    <Icon name={icon} size={24} color={tintColor} />
+                    <Icon name={icon} size={24} color={tintColor}/>
                 )}
                 <Text
                     numberOfLines={1}
-                    style={[styles.drawerRowTextStyle, { color: tintColor }]}
+                    style={[styles.drawerRowTextStyle, {color: tintColor}]}
                 >
                     {label}
                 </Text>
             </View>
         </Pressable>
-    );
-};
+    )
+}
 
 const getActiveRouteState = (routes: any[], index: number, name: string) =>
-    routes[index].name.toLowerCase().indexOf(name.toLowerCase()) >= 0;
+    routes[index].name.toLowerCase().indexOf(name.toLowerCase()) >= 0
 
-const DrawerContent: React.FC<DrawerContentComponentProps<
-    DrawerContentOptions
-    >> = (props) => {
-    const window = useWindowDimensions();
-    const rowWidth = (window.width * 0.75 * 80) / 100;
+const DrawerContent: React.FC<DrawerContentComponentProps<DrawerContentOptions>> = (props) => {
+    const window = useWindowDimensions()
+    const rowWidth = (window.width * 0.75 * 80) / 100
     const rotate = Animated.interpolate(props.progress, {
         inputRange: [0, 1],
         outputRange: [0.3, 0],
-    });
+    })
     const scale = Animated.interpolate(props.progress, {
         inputRange: [0, 1],
         outputRange: [0.9, 1],
-    });
+    })
     const translateX = Animated.interpolate(props.progress, {
         inputRange: [0, 1],
         outputRange: [-rowWidth, 0],
-    });
+    })
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ padding: 16, marginTop: 30 }}>
+        <SafeAreaView style={{flex: 1}}>
+            <View style={{padding: 16, marginTop: 30}}>
                 <Animated.View
                     style={[
                         styles.drawerAvatarStyle,
                         styles.avatarShadow,
-                        { transform: [{ rotate, scale }] },
+                        {transform: [{rotate, scale}]},
                     ]}
                 >
                     <Animated.Image
                         style={[
                             styles.drawerAvatarStyle,
-                            { transform: [{ rotate, scale }] },
+                            {transform: [{rotate, scale}]},
                         ]}
                         source={AppImages.userImage}
                     />
                 </Animated.View>
                 <Text style={styles.userName}>Chris Hemsworth</Text>
             </View>
-            <View style={styles.divider} />
+            <View style={styles.divider}/>
             <DrawerContentScrollView
                 {...props}
-                contentContainerStyle={{ flexGrow: 1, paddingTop: 0 }}
+                contentContainerStyle={{flexGrow: 1, paddingTop: 0}}
             >
                 <DrawerItemRow
                     label="Home"
                     icon="home"
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.navigate('Home')}
                 />
                 <DrawerItemRow
                     label="Help"
                     icon={AppImages.support_icon}
                     isAssetIcon
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.navigate('Help')}
                 />
                 <DrawerItemRow
                     label="Feedback"
                     icon="help"
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.navigate('Feedback')}
                 />
                 <DrawerItemRow
                     label="Invite Friend"
                     icon="group"
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.navigate('Invite Friend')}
                 />
                 <DrawerItemRow
                     label="Rate the app"
                     icon="share"
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
                 />
                 <DrawerItemRow
                     label="About Us"
                     icon="info"
-                    {...{ ...props, translateX }}
+                    {...{...props, translateX}}
                     onpress={() => props.navigation.dispatch(DrawerActions.closeDrawer())}
                 />
             </DrawerContentScrollView>
 
             <Pressable
-                style={({ pressed }) => [
+                style={({pressed}) => [
                     styles.signOutBtnStyle,
-                    { opacity: !Config.isAndroid && pressed ? 0.4 : 1 },
+                    {opacity: !Config.isAndroid && pressed ? 0.4 : 1},
                 ]}
-                android_ripple={{ color: 'lightgrey' }}
+                android_ripple={{color: 'lightgrey'}}
             >
                 <Text
-                    style={{ flex: 1, fontSize: 16, fontFamily: 'WorkSans-SemiBold' }}
+                    style={{flex: 1, fontSize: 16, fontFamily: 'WorkSans-SemiBold'}}
                 >
                     Sign Out
                 </Text>
-                <Icon name="power-settings-new" size={20} color="red" />
+                <Icon name="power-settings-new" size={20} color="red"/>
             </Pressable>
         </SafeAreaView>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
     userName: {
@@ -243,6 +239,6 @@ const styles = StyleSheet.create({
         borderTopWidth: StyleSheet.hairlineWidth,
         borderColor: 'darkgrey',
     },
-});
+})
 
-export default DrawerContent;
+export default DrawerContent
--- src/HomeScene.tsx
+++ src/HomeScene.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import {Text} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
+import {useHeaderHeight} from '@react-navigation/stack'
+import React, {useState} from 'react'
+import {Platform, Pressable, SafeAreaView, StatusBar, Text, View} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Config from './Config'
 
 
 interface Props{}
 
 const HomeScene: React.FC<Props> = () => {
 
+    const headerHeight = useHeaderHeight();
+    const navigation = useNavigation<any>();
+    const [isGrid, setGrid] = useState(true);
+
+    const marginTop = Platform.OS === "ios" ? 20 : StatusBar.currentHeight;
+
     return(
-        <Text>HomeScene</Text>
+       <SafeAreaView style={{flex : 1, marginTop}} >
+           <View
+               style={{flexDirection: "row", padding: 8}}
+           >
+               <Pressable
+                   style={({pressed}) => [
+                       {padding : 8, opacity: !Config.isAndroid && pressed ? 0.6 : 1},
+                   ]}
+                   onPress={() => navigation.toggleDrawer()}
+                   android_ripple={{color:"grey", radius: 20, borderless: true}}
+               >
+                   <Icon name="menu" size={25} color="black"/>
+               </Pressable>
+           </View>
+       </SafeAreaView>
     );
 }
 
